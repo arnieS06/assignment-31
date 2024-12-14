a660748e40cdb38c0aa6ad02c7a05c1d86e577b1
arnieS06 <sharma.arnav2016@gmail.com>
2024-12-13 18:25:37 -0800

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,22 +1,45 @@
 #include "Time.h"
-#include <iostream>
 using namespace std;
 
 Time::Time(int hour, int minute, int second) {
-	this->hour = hour;
-	this->minute = minute;
-	this->second = second;
+    this->hour = hour;
+    this->minute = minute;
+    this->second = second;
 }
 
-int Time::getHour()const {
-	return hour;
+int Time::getHour() const {
+    return hour;
 }
 
-int Time::getMinute()const {
-	return minute;
+int Time::getMinute() const {
+    return minute;
 }
 
-int Time::getSecond()const {
-	return second;
+int Time::getSecond() const {
+    return second;
 }
 
+void Time::addSeconds(int seconds) {
+    second += seconds;
+    while (second >= 60) {
+        second -= 60;
+        minute++;
+    }
+    addMinutes(0); 
+}
+
+void Time::addMinutes(int minutes) {
+    minute += minutes;
+    while (minute >= 60) {
+        minute -= 60;
+        hour++;
+    }
+    addHours(0); 
+}
+
+void Time::addHours(int hours) {
+    hour += hours;
+    while (hour >= 24) {
+        hour -= 24;
+    }
+}
