be374a37582f4a1009b62f4a552997c66f2f2319
arnieS06 <sharma.arnav2016@gmail.com>
2024-12-13 18:24:58 -0800

    Update Time.h

--- Time.h
+++ Time.h
@@ -5,16 +5,16 @@ using namespace std;
 
 class Time {
 public:
-	Time(int hour, int minute, int second);
-	int getHour()const;
-	int getMinute()const;
-	int getSecond()const;
-	void addSeconds(int seconds);
-	void addMinutes(int minutes);
-	void addHours(int hours);
+   Time(int hour, int minute, int second);
+   int getHour() const;
+   int getMinute() const;
+   int getSecond() const;
+   void addSeconds(int seconds); 
+   void addMinutes(int minutes); 
+   void addHours(int hours);     
 
 private:
-	int hour;
-	int minute;
-	int second;
+   int hour;
+   int minute;
+   int second;
 };
