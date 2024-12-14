ad78dc7701704ed99a6ff183c6d8fc13966f9c19
arnieS06 <sharma.arnav2016@gmail.com>
2024-12-13 18:30:54 -0800

    added one example of DateTime implementation

--- main.cpp
+++ main.cpp
@@ -4,8 +4,18 @@
 using namespace std;
 
 int main() {
+   Time time(23, 50, 45); 
+   cout << "Initial Time: " << time.getHour() << ":" << time.getMinute() << ":" << time.getSecond() << endl;
 
 
+   time.addSeconds(30); 
+   cout << "After adding 30 seconds: " << time.getHour() << ":" << time.getMinute() << ":" << time.getSecond() << endl;
+   
+   time.addMinutes(15); 
+   cout << "After adding 15 minutes: " << time.getHour() << ":" << time.getMinute() << ":" << time.getSecond() << endl;
 
-    return 0;
+   time.addHours(2);
+   cout << "After adding 2 hours: " << time.getHour() << ":" << time.getMinute() << ":" << time.getSecond() << endl;
+
+   return 0;
 }
